555c7e815609ac2207c3b4ad314e5f94c616e134
parsa panahpoor <parsapanahpoor77@gmail.com>
2024-11-19 11:54:11 +0330

    #1

---
+++ src/Framework/Domain/Framework.Domain.DependencyInjection/FrameworkDomainConfigurations.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using NetCore.AutoRegisterDi;
+
+namespace Framework.Domain.DependencyInjection;
+
+public static class FrameworkDomainConfigurations
+{
+	public static readonly Assembly[] Assemblies =
+	[
+		//IdentityDomainAssemblyReference.Assembly,
+	];
+
+	public static IServiceCollection RegisterDomainLayer(this IServiceCollection services)
+	{
+		services.RegisterAssemblyPublicNonGenericClasses(Assemblies)
+			.Where(c => c.Name.EndsWith("Service"))
+			.AsPublicImplementedInterfaces(ServiceLifetime.Scoped);
+
+		foreach (Assembly assembly in Assemblies)
+		{
+			services.AddValidatorsFromAssembly(assembly);
+		}
+
+		//services.AddAutoMapper(Assemblies);
+		return services;
+	}
+
+	public static IServiceProvider UseDomainLayer(this IServiceProvider app)
+		=> app;
+}
