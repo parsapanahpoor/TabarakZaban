ac9ee1f85d90894f00b0a41d5b678caf3343a718
parsa panahpoor <parsapanahpoor77@gmail.com>
2024-11-24 15:18:56 +0330

    #3

---
+++ src/Presentation/WebApp/ViewComponents/Header.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace WebHost.ViewComponents;
+
+public class HeaderViewComponent : ViewComponent
+{
+    public async Task<IViewComponentResult> InvokeAsync(CancellationToken cancellationToken)
+        => View("Header");
+}
